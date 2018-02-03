e6bde434902b317b9505480a91cb83efc318f3a1
Ryan Moore <rmoore8869@gmail.com>
2018-02-03 00:06:19 -0600

    fix: changed web.py

--- web.py
+++ web.py
@@ -1,3 +1,6 @@
+import os
+
+
 from sanic import Sanic
 from sanic.response import json
 
@@ -8,4 +11,9 @@ async def test(request):
     return json({'hello': 'world'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', 
+            port=int(os.environ.get('PORT', 8000)),
+            workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
+            debug=bool(os.environ.get('DEBUG', '')))
+
+
